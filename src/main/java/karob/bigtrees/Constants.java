2dd0b0cd3ccc27c5aba4218446ec69b6771c5302
Vidar Johansen <vidar@vidarjohansen.com>
2016-01-05 20:39:42 +0100

    Factored out properties to build.properties
    Added config file to easier manage the build. Automatic injection of
    version into Constants. Borrowed some gradle code from @Vazkii.

---
+++ src/main/java/karob/bigtrees/Constants.java
@@ -0,0 +1,10 @@
+package karob.bigtrees;
+
+public class Constants {
+	public static final String ModId = "bigtrees";
+
+	public static final String ModName = "BigTrees";
+
+	public static final String ModVersion = "GRADLE:VERSION";
+
+}
